dd6a00e3f23f23fde63ba822032ed14b936f8bae
ARahman360 <124456324+ARahman360@users.noreply.github.com>
2026-09-14 14:15:43 +0300

    More works

--- W1/A1_T1.py
+++ W1/A1_T1.py
@@ -1,3 +1,2 @@
 print("Hello, Python programming!")
-name = input("What is your name? ")
-print(f"Nice to meet you, {name}!")
+
---
+++ W1/A1_T5.py
@@ -0,0 +1,8 @@
+print("Calculate the area of a wall.")
+print("Enter the width of the wall in meters:")
+width = float(input())
+print("Enter the height of the wall in meters:")    
+height = float(input())
+print(f"{width} is the width of the wall and {height} is the height of the wall.")
+area = width * height
+print(f"The area of the wall is {area} square meters.")
