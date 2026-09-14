96433b48be611407d800af8273299fd4ccad1283
ARahman360 <124456324+ARahman360@users.noreply.github.com>
2026-09-14 23:45:07 +0300

    removed file

--- Learn_java/FirstJava.js
+++
@@ -1,6 +0,0 @@
-name = input("Please enter your name: ")
-print("Hello, " + name)
-print("Welcome to Java programming!")
-age = input("Please enter your age: ")
-print("You are " + age + " years old.")
-
