37d4bce3334ef81a932050689bcb4e1abf14a951
ARahman360 <124456324+ARahman360@users.noreply.github.com>
2026-09-14 21:39:24 +0300

    2nd branch

---
+++ Learn_java/FirstJava.js
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+print("Hello, " + name)
+print("Welcome to Java programming!")
+age = input("Please enter your age: ")
+print("You are " + age + " years old.")
+
